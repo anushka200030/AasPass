1abd9c637e02868b12f3b38fcaa4e2be654fa867
Rohit Raj <rohit3374@gmail.com>
2021-06-29 00:18:50 +0530

    "Added Onboarding Classes and Activities using Tushar codes"

---
+++ app/src/main/java/com/example/aaspass/onboarding/onboard3.java
@@ -0,0 +1,14 @@
+package com.example.aaspass.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.aaspass.R;
+public class onboard3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboard3);
+    }
+}
---
+++ app/src/main/java/com/example/aaspass/onboarding/onboard4.java
@@ -0,0 +1,16 @@
+package com.example.aaspass.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aaspass.R;
+
+public class onboard4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboard4);
+    }
+}
